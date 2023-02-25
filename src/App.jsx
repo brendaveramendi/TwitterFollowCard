6da3c3d0eb379c2a2620bc9e73d7d06bdf74db8f
Brenda Veramendi <brendaveramendi@gmail.com>
2023-02-25 16:06:14 -0300

    corection eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,45 @@
-import "./App.css";
-import { TwitterFollowCard } from "./TwitterFollowCard";
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 export const App = () => {
-  
   const user = [
     {
-      
-      name: "Miguel Ángel Duran",
-      userName: "midudev",
-      initialIsFollowing: true,
+
+      name: 'Miguel Ángel Duran',
+      userName: 'midudev',
+      initialIsFollowing: true
     },
     {
-   
-      name: "Binance",
-      userName: "binance",
-      initialIsFollowing: true,
+
+      name: 'Binance',
+      userName: 'binance',
+      initialIsFollowing: true
     },
     {
-      
-      name: "CryptoYoda",
-      userName: "CryptoYodaGuru",
-      initialIsFollowing: false,
+
+      name: 'CryptoYoda',
+      userName: 'CryptoYodaGuru',
+      initialIsFollowing: false
     },
     {
-     
-      name: "CNN en Español",
-      userName: "CNNEE",
-      initialIsFollowing: false,
-    },
-  ];
+
+      name: 'CNN en Español',
+      userName: 'CNNEE',
+      initialIsFollowing: false
+    }
+  ]
 
   return (
     <>
       <h5 className="tw-followCard-info">A quién seguir</h5>
       <section className="App">
         {
-          user.map(({name,userName,initialIsFollowing})=>(
-            <TwitterFollowCard key={userName}  name={name} userName={userName} initialIsFollowing={initialIsFollowing}  />
-            )
+          user.map(({ name, userName, initialIsFollowing }) => (
+            <TwitterFollowCard key={userName} name={name} userName={userName} initialIsFollowing={initialIsFollowing} />
+          )
           )
         }
-       
+
       </section>
     </>
-  );
-};
+  )
+}
